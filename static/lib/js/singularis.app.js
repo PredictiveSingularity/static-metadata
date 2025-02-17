9bac580e485b1739b3919d9b371130ba94fe58b3
wasertech <danny@waser.tech>
2025-02-17 18:35:20 +0100

    use rpc proxy

--- static/lib/js/singularis.app.js
+++ static/lib/js/singularis.app.js
@@ -34,6 +34,37 @@ const welcomeInstructionSchema = new Map([
 
 // Functions
 
+// Récupérer le JWT depuis le backend
+async function getToken() {
+    const response = await fetch("http://localhost:8000/get-token", { method: "POST" });
+    const data = await response.json();
+    console.log("Got token: ", data.token);
+    localStorage.setItem("jwt", data.token);
+}
+
+// Fonction pour envoyer une requête RPC avec JWT
+async function customFetch(url, options) {
+    console.log("Checking for JTW token...");
+    const token = localStorage.getItem("jwt");
+    if (!token) {
+        console.error("No JWT token!");
+        return;
+    } else {
+        console.log("Found token; requesting data...");
+    }
+
+    const response = await fetch("http://localhost:8000/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: options.body
+    });
+
+    return response.json();
+}
+
 async function findTokenAccountAddress(mintPublicKey, ownerPublicKey) {
     // Define the seeds
     const seeds = [
@@ -155,7 +186,9 @@ async function connectWallet() {
             console.log("Energy Account Address: ", energyAccountValue.toBase58());
             if (energyAccountValue!== "1e") {
                 const networkDropdown = document.querySelector("#network > div.container > div > div.wrap-inner > div > input");
-                const network = networkDropdown.value;
+                const network = 'https://rpc.api.singularicula.me'; //networkDropdown.value;
+                // getToken();
+                // Création d'une instance de Connection qui utilise customFetch
                 const connection = new solanaWeb3.Connection(network, 'confirmed');
                 console.log("Connectcting to network: ", network);
                 connection.getParsedAccountInfo(energyAccountValue).then(async (accountInfo) => {
@@ -334,7 +367,8 @@ async function transform(transformer_energy, transformer_address, hf_models, hf_
             // Check if transformer is initialized
     const transformerPublicKey = new solanaWeb3.PublicKey(transformer);
     const networkDropdown = document.querySelector("#network > div.container > div > div.wrap-inner > div > input");
-    const network = networkDropdown.value;
+    const network = 'https://rpc.api.singularicula.me'; //networkDropdown.value;
+    // getToken();
     const connection = new solanaWeb3.Connection(network, 'confirmed');
     console.log("Connectcting to network: ", network);
     const accountInfo = await connection.getParsedAccountInfo(transformerPublicKey) //.then(async (accountInfo) => {
@@ -462,7 +496,8 @@ function encodeU64(value) {
 
 async function checkMetabolizerAndProvisionEnergy() {
     console.log("Checking your metabolizer account...");
-    const network = document.querySelector("#network > div.container > div > div.wrap-inner > div > input").value;
+    const network = 'https://rpc.api.singularicula.me'; //document.querySelector("#network > div.container > div > div.wrap-inner > div > input").value;
+    // getToken();
     const connection = new solanaWeb3.Connection(network, 'confirmed');
     console.log("Connectcting to network: ", network);
     const metabolizer_address = document.querySelector("#metabolizer-address > label > div.input-container > textarea").value;
@@ -677,6 +712,7 @@ function sendMessage() {
 //         const provider = window.solana;
 //         if (provider.isPhantom) {
 //             try {
+                    // getToken();
 //                 const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
 //                 const fromPubkey = provider.publicKey;
 //                 const toPubkey = new solanaWeb3.PublicKey(recipient);
